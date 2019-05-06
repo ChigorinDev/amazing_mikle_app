1bce094a07590709843d68690ef7e54cd769dad1
Chigorin M.L <mmchigorin@gmail.com>
2019-05-06 11:32:33 +0300

    add layout components

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SignInLinks from './SignInLinks';
+
+const Navbar = () => { 
+  return (
+    <nav className="nav-wrapper grey darken-3">
+      <div className="container">
+        <Link to='/' className='brand-logo'>MikleAmazingApp</Link>
+        <SignInLinks />
+      </div>
+    </nav>
+  )
+}
+
+
+export default Navbar;
