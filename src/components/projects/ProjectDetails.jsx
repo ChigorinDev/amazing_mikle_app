9a9ede2112a50de1e2a0ccf14cda09d4e5e35647
Chigorin M.L <mmchigorin@gmail.com>
2019-05-06 13:45:11 +0300

    add sign in and sign up forms

--- src/components/projects/ProjectDetails.jsx
+++ src/components/projects/ProjectDetails.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const ProjectDetails = () => {
+const ProjectDetails = (props) => {
+  const id = props.match.params.id;
   return (
     <div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
           <span className="card-title">
-            Project Title
+            Project Title (id: {id})
           </span>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque odit eum aliquam,
              laborum autem architecto nisi at dolores.
